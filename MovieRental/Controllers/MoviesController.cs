94a33d11f76d0e9dd65cd8286eb07b093ae3cf5c
barteksmolec12 <barteksmolec@o2.pl>
2020-06-17 19:58:47 +0200

    route config

---
+++ MovieRental/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRental.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year+"/"+month );
+        }
+    }
+}
