3943f51a98edf66cab42189e156c55cf960f4765
barteksmolec12 <barteksmolec@o2.pl>
2020-06-25 22:54:23 +0200

    movies_and_customers_controllers

--- MovieRental/Controllers/MoviesController.cs
+++ MovieRental/Controllers/MoviesController.cs
@@ -10,41 +10,50 @@ namespace MovieRental.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies
-        [Route("movies/random")]
-        public ActionResult Random()
-        {
+        //[Route("customers")]
+        //public ActionResult Random()
+        //{
 
-            var movie = new Movie() { Name = "Shrek" };
-            var customers = new List<Customer>
-            {
-	            new Customer {Name = "Customer1"},
-	            new Customer {Name = "Customer2"}
+        //    var movie = new Movie() { Name = "Shrek" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer1"},
+        //        new Customer {Name = "Customer2"}
 
 
-            };
+        //    };
 
-            var viewModel = new RandomMovieViewModel()
-            {
-	            Movie = movie,
-	            Customers = customers
-            };
+        //    var viewModel = new RandomMovieViewModel()
+        //    {
+        //     Movie = movie,
+        //     Customers = customers
+        //    };
 
-            return View(viewModel);
-        }
+        //    return View(viewModel);
+        //}
 
-        [Route("movies/released/{year}/{month}")]
-        public ActionResult ByReleaseYear(int year, int month)
-        {
-            return Content(year+"/"+month );
-        }
+        //[Route("movies/released/{year}/{month}")]
+        //public ActionResult ByReleaseYear(int year, int month)
+        //{
+        //    return Content(year+"/"+month );
+        //}
 
-        [Route("movies/test")]
-        public ActionResult Test()
-        {
-            return Content("hej !");
-
-        }
 
     }
 }
--- MovieRental/Models/Customer.cs
+++ MovieRental/Models/Customer.cs
@@ -7,7 +7,10 @@ namespace MovieRental.Models
 {
 	public class Customer
 	{
-		public int Id { get; set; }				
+		public int Id
+		{
+			get; set;
+		}				
 		public string Name { get; set; }
 	}
 }
